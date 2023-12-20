d34ba9a2033d31a183f39f3f5ad317ba4dca2f89
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-12-20 15:10:02 +0330

    config: first config of app

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+
+
+
+const Layout = () => {
+    return ( 
+        <>
+       
+        <main>
+        <Outlet />
+        </main>
+        
+        </>
+     );
+}
+ 
+export default Layout;
---
+++ src/Context/Providers.jsx
@@ -0,0 +1,18 @@
+
+
+// import React from "react";
+// import { UserProvider } from "./UsersContext";
+// import { OpenProvider } from "./OpenContext";
+// import { UsersReducerProvider } from "./UsersContextReducer";
+
+
+// function Providers({ children }) {
+//   return (
+//     <UserProvider>
+//       <OpenProvider>{children}</OpenProvider>
+//       <UsersReducerProvider></UsersReducerProvider>
+//     </UserProvider>
+//   );
+// }
+
+// export default Providers;
