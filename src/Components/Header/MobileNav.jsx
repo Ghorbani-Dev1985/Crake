f0c68089523da72de72a7b65f35d26cb6daabec5
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2023-12-25 19:41:20 +0330

    add: mui install

--- src/Components/Header/MobileNav.jsx
+++ src/Components/Header/MobileNav.jsx
@@ -2,6 +2,19 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BiMenuAltRight } from "react-icons/bi";
 import Logo from "./../../assets/Images/logo.png";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 const MenuItems = [
   {
     id: 1,
@@ -36,10 +49,76 @@ const MenuItems = [
 ];
 
 function MobileNav() {
-  const [toggleMobileNav , setToggleMobileName] = useState(false)
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
     <>
-      {
+       <AppBar className="static block md:hidden bg-mainColor border-b border-b-white/20">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              className="w-full"
+            >
+              {MenuItems.map(({id , title , link}) => (
+                <MenuItem key={id} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{title}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Link
+            to="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src={Logo}
+              alt="ghorbani-dev.ir"
+              className="w-36 h-auto object-cover"
+            />
+          </Link>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+      {/* {
         toggleMobileNav &&
         <div onClick={() => setToggleMobileName(!toggleMobileNav)}
            className="w-full h-full bg-purple-200/20 fixed top-0 left-0 right-0 mx-auto backdrop-blur-sm transition-all z-40"></div>
@@ -99,7 +178,7 @@ function MobileNav() {
 
         </div>
         </div>
-      </nav>
+      </nav> */}
     </>
   );
 }
