25d1d64dbe8de9b9414dba7799a17d5dfd78f7aa
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2023-12-27 22:08:13 +0330

    feat: develop img section and service component

--- src/Components/Pages/HomePage/HomePage.jsx
+++ src/Components/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import OurFeatures from './OurFeatures'
+import PanelServices from './PanelServices/PanelServices'
 
 function HomePage() {
   return (
     <>
     <OurFeatures />
+    <PanelServices />
     </>
   )
 }
--- src/Components/Pages/HomePage/PanelServices/PanelServices.jsx
+++ src/Components/Pages/HomePage/PanelServices/PanelServices.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Feature from '../../../../assets/Images/features-img1.png'
 
 function PanelServices() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section>
+      <div className="container">
+        <div className="flex-between">
+          <div className="flex flex-1">
+           <img src={Feature} alt="ghorbani-dev.ir" />
+          </div>
+          <div className="flex flex-1"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default PanelServices
+export default PanelServices;
+
+
+function Service(){
+    return(
+        <div className="flex flex-col items-center">
+           
+        </div>
+    )
+}
+
+export {Service};
