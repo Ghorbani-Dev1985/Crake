7a391a6a6d4843da433ac4bbccb0c53c45d0022d
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-02 19:18:08 +0330

    featBackend: develop routes and userRoutes and complete insert query

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,12 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const crakeDB = require('./DB/crakeDB')
+const usersRoutes = require('./Routes/userRoutes')
 
-crakeDB.connect((error) => {
-    if(error) {
-        console.log('Connection Unsuccessfully...')
-    }else{
-        console.log('Connection Successfully...')
-        let newUserInsertQuery = "INSERT INTO `users` VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')"
-    }
-})
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/users' , usersRoutes);
